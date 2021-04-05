a1822bbe2e1ed01fb77f2fdc0bb5223173b2c375
Sam A. Horvath-Hunt <hello@samhh.com>
2021-04-05 13:42:32 +0100

    Apply lint fixes + minor refactoring

--- bangin.sh
+++ bangin.sh
@@ -3,14 +3,14 @@
 # bangin - a primitive, portable shell script which enables DuckDuckGo-like bangs.
 # Version: 0.1.1
 
-input=${1}
+readonly input=${1}
 
-search=${input%!*}
+readonly search=${input%!*}
 
 # Disallows no search text before bang
 if [ -z "$search" ]; then exit 1; fi
 
-bang_text=${input##*!}
+readonly bang_text=${input##*!}
 
 # Disallows empty bang
 if [ -z "$bang_text" ]; then exit 1; fi
@@ -18,10 +18,10 @@ if [ -z "$bang_text" ]; then exit 1; fi
 # Disallows no bang
 if [ "$bang_text" = "$input" ]; then exit 1; fi
 
-bang="!$bang_text"
+readonly bang="!$bang_text"
 
 find_bang_in_file() {
-    echo "$(sed -n "/^$bang /s/^$bang //p" "$1" 2> /dev/null)"
+    sed -n "/^$bang /s/^$bang //p" "$1" 2> /dev/null
 }
 
 # Check user's config first
@@ -30,6 +30,7 @@ template=$(find_bang_in_file "${XDG_CONFIG_HOME:-$HOME/.config}/bangin/bangin.ba
 # We need to check the files in reverse alphabetical order. The things we do
 # for portability...
 set -- "${XDG_DATA_HOME:-$HOME/.local/share}"/bangin/lists/*.bangs
+file=
 i=$#
 while [ $i -gt 0 ]; do
     if [ "$template" ]; then break; fi
@@ -43,20 +44,20 @@ if [ -z "$template" ]; then exit 1; fi
 
 # From: https://unix.stackexchange.com/a/60698
 urlencode () {
-  string=$1
-  while [ -n "$string" ]; do
-    tail=${string#?}
-    head=${string%$tail}
-    case $head in
-      [-._~0-9A-Za-z]) printf %c "$head";;
-      *) printf %%%02x "'$head"
-    esac
-    string=$tail
-  done
-  echo
+    string=$1
+    while [ -n "$string" ]; do
+        tail=${string#?}
+        head=${string%$tail}
+        case $head in
+            [-._~0-9A-Za-z]) printf %c "$head";;
+            *) printf %%%02x "'$head"
+        esac
+        string=$tail
+    done
+    echo
 }
 
-replaced=$(echo "$template" | sed "s/{{{s}}}/$(urlencode "$search")/")
+readonly replaced=$(echo "$template" | sed "s/{{{s}}}/$(urlencode "$search")/")
 
 echo "$replaced"
 
